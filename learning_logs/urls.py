25f02ddf5764d30a401cb66c3abe379a4b177018
Chidi <chidi990@hotmail.com>
2018-05-30 13:21:01 +0100

    Add new / edit existing entries, user accounts associated with entries, create new user, restrict topics page to owners.

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -12,5 +12,15 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
     
     # Detail page for a single topic
-    path('topics/(?P/<topic_id>\d+)/$', views.topic, name='topic'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    
+    # Page for adding a new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
+    
+    # Page for adding a new entry
+    path('new_entry/<topic_id>/', views.new_entry, name='new_entry'),
+    
+    # Page for editing an entry
+    path('edit_entry/<topic_id>/', views.edit_entry, 
+            name='edit_entry'),
 ]
---
+++ users/urls.py
@@ -0,0 +1,20 @@
+"""Defines URL patterns for users"""
+
+from django.urls import path
+from django.contrib.auth.views import login
+
+from . import views
+
+urlpatterns = [
+    # Login page
+    path('login/', login, {'template_name': 'users/login.html'},
+            name='login'),
+    # Logout page
+    path('logout/', views.logout_view, name='logout'),
+    # Registration page
+    path('register/', views.register, name='register')
+    
+]
+
+
+        
